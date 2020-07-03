54767d4e11d0a423a2ed9a7dee0ff3b097a2432e
Palenov Yaroslav <palenov.yaroslav@gmail.com>
2020-07-03 16:10:29 +0300

    init

---
+++ precache-manifest.0a6561a3a125a3a5120747d5f90174a7.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc3743d80cfc2490538e4689a0430285",
+    "url": "/index.html"
+  },
+  {
+    "revision": "83ec9b724ccc1f54fc02",
+    "url": "/static/css/2.2b2b3b2a.chunk.css"
+  },
+  {
+    "revision": "e4e6f7eef88bf09b6e54",
+    "url": "/static/css/main.139f728c.chunk.css"
+  },
+  {
+    "revision": "83ec9b724ccc1f54fc02",
+    "url": "/static/js/2.f923dcea.chunk.js"
+  },
+  {
+    "revision": "bfc08a62983de46a4daec9f8c3c657d8",
+    "url": "/static/js/2.f923dcea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e4e6f7eef88bf09b6e54",
+    "url": "/static/js/main.07a70150.chunk.js"
+  },
+  {
+    "revision": "45e7d3f6d3552886a0f0",
+    "url": "/static/js/runtime-main.5ea727db.js"
+  },
+  {
+    "revision": "8333048eaf49e9ca942434334ffe7732",
+    "url": "/static/media/bankex.8333048e.png"
+  },
+  {
+    "revision": "f4ac1cf384b97853758eeef7a69e707f",
+    "url": "/static/media/bot.f4ac1cf3.gif"
+  },
+  {
+    "revision": "474dff81c4eac0f29e7080618ec975e6",
+    "url": "/static/media/onpoint.474dff81.gif"
+  },
+  {
+    "revision": "d6e3596c84c90e093b7e1ea296251b17",
+    "url": "/static/media/screen.d6e3596c.png"
+  },
+  {
+    "revision": "c3cc8fc978a75132db7c32b4185f671d",
+    "url": "/static/media/yandex.c3cc8fc9.jpeg"
+  }
+]);
